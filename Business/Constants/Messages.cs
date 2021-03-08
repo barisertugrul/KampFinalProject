aaecfea115f09491b15835ce99d0ab8aca600334
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-03-08 19:22:12 +0300

    13.Gün Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,11 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductListed = "Ürünler listelendi";
+        public static string UnitPriceInvalid = "Ürün fiyatı 0'a eşit veya 0'dan büyük olmalı";
+        public static string ProductNameStartWithA = "Ürün ismi A harfi ile başlamalı";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
     }
 }
